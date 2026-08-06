b22b0d7f498bb6ba4db89c0c174d78290cfd527d
adityaa250707-hue <adityaa250707@gmail.com>
2026-08-06 12:02:16 +0530

    dd

---
+++ learning c/SumOfTwoNumbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int x, y, sum;
+    printf("enter your first no.");
+    scanf("%d", &x);
+    printf("enter your second no.");
+    scanf("%d", &y);
+
+    sum = (x + y);
+        printf("sum of the no. is : %d", sum);
+
+            return 0;
+}
