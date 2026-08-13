e92c157d76ec3200cc59ddf05c0e520ca796fd82
adityaa250707-hue <adityaa250707@gmail.com>
2026-08-13 17:01:45 +0530

    .

--- learning c/2./CheckingTheSidesOfTriangle.c
+++ learning c/2./CheckingTheSidesOfTriangle.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-int main() {
-    int a,b,c;
+int main()
+{
+    int a, b, c;
     printf("enter the three sides of triangle");
-    scanf("%d %d %d",&a,&b,&c);
-    if (a+b>c && b+c>a && c+a>b)
-    {printf("valid triangle");}
-    else {printf("not a valid triangle");}
-    
-    
-    
+    scanf("%d %d %d", &a, &b, &c);
+    if (a + b > c && b + c > a && c + a > b)
+    {
+        printf("valid triangle");
+    }
+    else
+    {
+        printf("not a valid triangle");
+    }
+
     return 0;
-} 
+}
